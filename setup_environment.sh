27e6d77a811cc7a8efef39df8a76d7a15092d533
Tarun Gunaseelan <tarun.gunaseelan@gmail.com>
2023-08-23 17:04:28 -0400

    Switch to setup with mamba for speed

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,10 +5,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Initialize conda for shell interaction
-eval "$(/software/neurodev/anaconda3/bin/conda shell.bash hook)"
+eval "$($(which conda || echo /software/neurodev/anaconda3/bin/conda) shell.bash hook)"
+
+# Check if mamba is installed
+if ! command -v mamba &> /dev/null
+then
+    $(which conda || echo /software/neurodev/anaconda3/bin/conda) install mamba -c conda-forge -y
+fi
 
 # Set up the conda environment using the environment.yml file located in the current directory
-conda env create -f $DIR/environment.yml
+mamba env create -f $DIR/environment.yml
 
 # Source the conda profile script to ensure conda commands are available
 source /software/neurodev/anaconda3/etc/profile.d/conda.sh
